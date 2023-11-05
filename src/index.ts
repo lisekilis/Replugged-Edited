00cbdc574ea3c88ead3eaa084a553c9ae7512976
lisekilis <101191226+lisekilis@users.noreply.github.com>
2023-11-05 19:42:34 +0100

    Deleted more useless stuff

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,11 @@ const inject = new Injector();
 const logger = Logger.plugin("Edited");
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
   const getChannelMod = await webpack.waitForModule<{
     getChannel: (id: string) => {
       name: string;
     };
   }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(
-        `Typing prevented! Channel: #${
-          channelObj?.name ?? "unknown"
-        } (${channel}).`
-      );
-    });
-  }
 }
 
 export function stop(): void {
