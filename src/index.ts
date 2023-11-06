2b23cb4367638278d9f375f079d797ed2b0c0e06
lisekilis <101191226+lisekilis@users.noreply.github.com>
2023-11-06 17:40:03 +0100

    Some trolling

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,25 @@ const inject = new Injector();
 const logger = Logger.plugin("Edited");
 
 export async function start(): Promise<void> {
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
+  const sendMessageMod = await webpack.waitForProps<{
+    sendMessage: (id: string) => {
       name: string;
     };
-  }>(webpack.filters.byProps("getChannel"));
+  }>("sendMessage");
+
+  if (sendMessageMod) {
+    inject.after(sendMessageMod, "sendMessage", (args, res) => {
+      if (!(res instanceof Promise)) {
+        logger.log("Not a Promise");
+        return res;
+      }
+
+      return res.then((httpres) => {
+        console.log("Response: ", httpres);
+        return httpres;
+      });
+    });
+  }
 }
 
 export function stop(): void {
