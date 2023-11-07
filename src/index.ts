4d26b84c2f1a226a6e860205b22d0100a4126cd5
lisekilis <lisekilisgewp@gmail.com>
2023-11-07 18:11:48 +0100

    Fixed some Lint issues

--- src/index.ts
+++ src/index.ts
@@ -17,17 +17,16 @@ export async function start(): Promise<void> {
         return res;
       }
 
-      return res.then((httpres: HTTPResponse) => {
+      return res.then((httpres: Response) => {
         // console.log("Response: ", httpres);
         const org_content = httpres.body.content;
         const edited = org_content.indexOf("(edited)");
         if (edited !== -1) {
           const msgid = httpres.body.id;
           const msgchid = httpres.body.channel_id;
-          const content =
-            org_content.slice(0, edited) +
-            "\u202B\u202B" +
-            org_content.slice(edited + "(edited)".length);
+          const content = `${org_content.slice(0, edited)}\u202B\u202B${org_content.slice(
+            edited + "(edited)".length,
+          )}`;
           sendMessageMod.editMessage(msgchid, msgid, { content });
         }
         return httpres;
