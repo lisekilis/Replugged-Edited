6af420a337166f7bec41fcceafe215c62a3a48b0
lisekilis <101191226+lisekilis@users.noreply.github.com>
2023-11-13 16:45:29 +0100

    Deleted some more useless stuff

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,23 @@ interface HTTPResponse<T = Record<string, unknown>> {
 
 export async function start(): Promise<void> {
   inject.after(messages, "sendMessage", (_args, res) => {
-    if (!(res instanceof Promise)) {
-      logger.log("Not a Promise");
-      return res;
-    }
-
-    return (res as Promise<HTTPResponse<Record<string, string>>>).then((httpres) => {
-      console.log("Response: ", httpres);
-      const org_content = httpres.body!.content;
-      const edited = org_content.indexOf("(edited)");
-      if (edited !== -1) {
-        const msgid = httpres.body!.id;
-        const msgchid = httpres.body!.channel_id;
-        const content = `${org_content.slice(0, edited)}\u202B\u202B${org_content.slice(
-          edited + "(edited)".length,
-        )}`;
-        messages.editMessage(msgchid, msgid, { content });
+    return (res as Promise<HTTPResponse<Record<string, string>>>).then(
+      (httpres) => {
+        console.log("Response: ", httpres);
+        const org_content = httpres.body!.content;
+        const edited = org_content.indexOf("(edited)");
+        if (edited !== -1) {
+          const msgid = httpres.body!.id;
+          const msgchid = httpres.body!.channel_id;
+          const content = `${org_content.slice(
+            0,
+            edited
+          )}\u202B\u202B${org_content.slice(edited + "(edited)".length)}`;
+          messages.editMessage(msgchid, msgid, { content });
+        }
+        return httpres;
       }
-      return httpres;
-    });
+    );
   });
 }
 
