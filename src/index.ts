c87c8d36ac9d9bcd9aa9898158e3e728910b462b
lisekilis <lisekilisgewp@gmail.com>
2024-01-31 21:58:32 +0100

    trying to implement a context menu item

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { Injector, common } from "replugged";
+import { Injector, common, components, types, Logger } from "replugged";
 import { cfg } from "./config";
 
 export * from "./settings";
 
-const inject = new Injector();
 const { messages } = common;
+const { MenuItem } = components.ContextMenu;
+const { ContextMenuTypes } = types;
+const { toast } = common;
+const inject = new Injector();
 const key = cfg.get("key", "(edited)");
+const logger = Logger.plugin("Replugged-Edited");
 
 interface HTTPResponse<T = Record<string, unknown>> {
   body: T;
@@ -14,22 +18,43 @@ interface HTTPResponse<T = Record<string, unknown>> {
   status: number;
   text: string;
 }
+function editMessage(msgchid: string, msgid: string, orgContent: string) {
+  const edited = orgContent.indexOf(key);
+  if (edited !== -1) {
+    const content = `${orgContent.slice(0, edited)}\u202B\u202B${orgContent.slice(
+      edited + key.length,
+    )}`;
+    return messages.editMessage(msgchid, msgid, { content });
+  }
+  return -1;
+}
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function start(): Promise<void> {
+  inject.utils.addMenuItem(
+    ContextMenuTypes.Message,
+    (data) => {
+      const item = {
+        id: "Edited",
+        label: "(Edit) Message",
+        action: () => {
+          if (editMessage(data.channel.id, data.message.id, data.message.content) == -1) {
+            toast.toast("No key found!", toast.Kind.FAILURE, { duration: 5000 });
+          } //logger.log(data.message.author) replace me,
+        },
+        type: MenuItem,
+      };
+      if (data.message.author == common.users.getCurrentUser()) {
+        return item;
+      }
+      return;
+    },
+    2,
+    -1,
+  );
   inject.after(messages, "sendMessage", (_args, res) => {
     return (res as Promise<HTTPResponse<Record<string, string>>>).then((httpres) => {
-      const orgContent = httpres.body.content;
-      const edited = orgContent.indexOf(key);
-      if (edited !== -1) {
-        const msgid = httpres.body.id;
-        const msgchid = httpres.body.channel_id;
-        const content = `${orgContent.slice(0, edited)}\u202B\u202B${orgContent.slice(
-          edited + key.length,
-        )}`;
-        void messages.editMessage(msgchid, msgid, { content });
-      }
-      return httpres;
+      return editMessage(httpres.body.channel_id, httpres.body.id, httpres.body.content);
     });
   });
 }
