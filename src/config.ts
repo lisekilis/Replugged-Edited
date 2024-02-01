aa622db6dac67ddb9d4e35d82664395600ec33c4
lisekilis <lisekilisgewp@gmail.com>
2024-02-01 13:40:18 +0100

    Added replacement toggle

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,12 @@ import { settings } from "replugged";
 
 interface Settings {
   key?: string;
+  toggle?: boolean;
 }
 
 const defaultSettings = {
   key: "(edited)",
+  toggle: true,
 } satisfies Partial<Settings>;
 
 export const cfg = await settings.init<Settings, keyof typeof defaultSettings>(
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { Injector, common, components, types, Logger } from "replugged";
+import { Injector, common, components, types } from "replugged";
 import { cfg } from "./config";
 
 export * from "./settings";
 
 const { messages } = common;
 const { MenuItem } = components.ContextMenu;
+const { MenuCheckboxItem } = components.ContextMenu;
 const { ContextMenuTypes } = types;
 const { toast } = common;
 const inject = new Injector();
 const key = cfg.get("key", "(edited)");
-const logger = Logger.plugin("Replugged-Edited");
 
 interface HTTPResponse<T = Record<string, unknown>> {
   body: T;
@@ -18,6 +18,7 @@ interface HTTPResponse<T = Record<string, unknown>> {
   status: number;
   text: string;
 }
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function editMessage(msgchid: string, msgid: string, orgContent: string) {
   const edited = orgContent.indexOf(key);
   if (edited !== -1) {
@@ -39,10 +40,10 @@ export async function start(): Promise<void> {
     }) => {
       const item = {
         id: "Edited",
-        label: "(Edit) Message",
+        label: `${key} Message`,
         action: () => {
           if (editMessage(data.channel.id, data.message.id, data.message.content) == -1) {
-            toast.toast("No key found!", toast.Kind.FAILURE, { duration: 5000 });
+            toast.toast("Key not found!", toast.Kind.FAILURE, { duration: 5000 });
           } //logger.log(data.message.author) replace me,
         },
         type: MenuItem,
@@ -50,26 +51,47 @@ export async function start(): Promise<void> {
       if (data.message.author.id == common.users.getCurrentUser().id) {
         return item;
       }
-      return;
     },
     2,
     -1,
   );
+
+  inject.utils.addMenuItem(ContextMenuTypes.TextareaContext, () => {
+    const toggle = cfg.get("toggle", true);
+    const item = {
+      id: "editedToggleOn",
+      label: `Automatic ${key} replacement`,
+      checked: toggle,
+      action: () => {
+        cfg.set("toggle", !toggle);
+        common.contextMenu.close();
+      },
+      type: MenuCheckboxItem,
+    };
+    return item;
+  });
+
   inject.after(messages, "sendMessage", (_args, res) => {
+    const toggle = cfg.get("toggle", true);
     return (res as Promise<HTTPResponse<Record<string, string>>>).then((httpres) => {
-      return editMessage(httpres.body.channel_id, httpres.body.id, httpres.body.content);
+      if (toggle) {
+        return editMessage(httpres.body.channel_id, httpres.body.id, httpres.body.content);
+      }
     });
   });
   inject.before(messages, "editMessage", (args) => {
-    const orgContent = args[2].content;
-    const edited = orgContent.indexOf(key);
-    if (edited !== -1) {
-      const content = `${orgContent.slice(0, edited)}\u202B\u202B${orgContent.slice(
-        edited + key.length,
-      )}`;
-      args[2].content = content;
+    const toggle = cfg.get("toggle", true);
+    if (toggle) {
+      const orgContent = args[2].content;
+      const edited = orgContent.indexOf(key);
+      if (edited !== -1) {
+        const content = `${orgContent.slice(0, edited)}\u202B\u202B${orgContent.slice(
+          edited + key.length,
+        )}`;
+        args[2].content = content;
+      }
+      return args;
     }
-    return args;
   });
 }
 
