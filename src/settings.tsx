e1ddbeff8dcdcb02cb3c39b765dffc543fe3524e
lisekilis <lisekilisgewp@gmail.com>
2024-02-02 22:53:00 +0100

    "The Settings" pt.3

--- src/settings.tsx
+++ src/settings.tsx
@@ -1,14 +1,32 @@
 import { components, settings, util } from "replugged";
-const { Text } = components;
-const { TextInput } = components;
+
+const { Clickable, Divider, FormItem, SwitchItem, Text, TextInput, Tooltip } = components;
 
 const cfg = await settings.init("dev.lisekilis.RepluggedEdited");
 
 export function Settings(): React.ReactElement {
   return (
     <>
-      <Text.H1>The key that gets replaced:</Text.H1>
-      <TextInput {...util.useSetting(cfg, "key", "(edited)")} />
+      <SwitchItem
+        {...{
+          note: "Replaces the Key when sending or editing messages.",
+          ...util.useSetting(cfg, "toggle", true),
+        }}>
+        Automatic Key replacement
+      </SwitchItem>
+      <Text.H2>Key:</Text.H2>
+      <FormItem title="It's the Key that gets replaced">
+        <Tooltip text="Pro tip: don't set it to a single letter">
+          {<TextInput {...util.useSetting(cfg, "key", "(edited)")} />}
+        </Tooltip>
+      </FormItem>
+      <Divider />
+      <Clickable
+        onClick={() =>
+          window.open("https://discord.com/vanityurl/dotcom/steakpants/flour/flower/index11.html")
+        }>
+        {<Text.H2>♪(^∇^*)</Text.H2>}
+      </Clickable>
     </>
   );
 }
